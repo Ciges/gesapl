dce190f415143b1f6e1f44552517977a400354c8
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2020-08-01 20:48:23 +0200

    Added command --arrancar_servicio

--- lib/gesapl/gesapl_functions.sh
+++ lib/gesapl/gesapl_functions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Funciones comunes a varios scripts de GesApl
+
 # Graba el mensaje pasado como parámetro en en log, precedido de la fecha y hora
 function log {
     now="$(date +'%b %e %T')"
@@ -9,4 +11,9 @@ function log {
 # Devuelve el listado de ficheros de configuración de servicios
 function services_configs {
 	find "${services_data}" -type f|grep -v '\.'|sort
+}
+
+# Devuelve el listado de servicios monitorizados
+function active_services {
+	for c in $(services_configs); do printf "%s " ${c##*/}; done
 }
