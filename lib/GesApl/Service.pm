2632d5466d56f11b4a6bbf421cf0a96ac862d11e
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2020-08-20 14:50:23 +0200

    Class name changed to Service. Read from config file added.

--- lib/GesApl/Service.pm
+++ lib/GesApl/Service.pm
@@ -3,5 +3,134 @@ package GesApl::Service;
 use strict;
 use warnings;
 
+# GesApl mmodules
+use GesApl::App;
+
+1;
+
+# Constructor
+sub new {
+    my $class = shift;
+
+    # This method is static
+    die "class method invoked on object" if ref $class;
+
+    my $self = bless {}, $class;
+
+    $self->_initialize(@_);
+    return $self;
+}
+
+sub _initialize {
+    my $self = shift;
+
+    my $service_name = shift;
+    if ($service_name) {
+        $self->{_name} = $service_name;
+        $self->load_config();
+    }
+}
+
+# Getters
+sub get_name {
+    my $self = shift;
+
+    return $self->{_name};
+}
+
+sub get_script {
+    my $self = shift;
+
+    return $self->{_script};
+}
+
+sub get_pid_file {
+    my $self = shift;
+
+    return $self->{_pid_file};
+}
+
+sub get_process {
+    my $self = shift;
+
+    return $self->{_process};
+}
+
+# Load config from text file
+sub load_config {
+    my $self = shift;
+
+    my $service_name = $self->get_name();
+
+    # Load config from file
+    my $config_filename = GesApl::App->get_cfg('services_data')."/".$service_name;
+    my $config_file;
+    open ($config_file, '<', $config_filename)
+        or die "Read of file $config_file impossible $!\n";
+    my $config = <$config_file>;
+    chomp ($config);
+
+    my @fields = split ",", $config;
+    $self->{_script} = $fields[0];
+    $self->{_pid_file} = $fields[1];
+    $self->{_process} = $fields[2];
+    close $config_file;
+
+    die "Error when readind config data from $config_filename $!\n" if (not defined($self->{_script}) or not defined($self->{_pid_file}) or not defined($self->{_process}) );
+
+}
+
+# Print instance properties
+sub print {
+    my $self = shift;
+
+    printf "Service name: %s\n", $self->get_name();
+    printf "Start/stop script name: /etc/init.d/%s\n", $self->get_script();
+    printf "Pid file: %s\n", $self->get_pid_file();
+    printf "Process name: %s\n", $self->get_process();
+}
 
 __END__
+
+# HELP
+
+=encoding utf8
+
+=head1 NOMBRE
+
+GesApl::Service - Modulo para gestionar la configuración de un servicio registrado en GesApl 2.00 (con ficheros de texto)
+
+=head1 SINOPSIS
+
+Este módulo, aunque puede ser usado directamente, está pensado para ser usado por la aplicación GesApl. 
+
+    # Carga de la configuración de Apache
+    my $apache_config = GesApl::Service->new("apache");
+
+    # Reload and show configuration from /etc/gesapl/services/apache
+    $apache_config->load_config();
+    $apache_config->print();
+
+=head1 DESCRIPCION
+
+GesApl es una aplicación que permite monitorizar distintos servicios del sistema.
+
+Este módulo se encarga de gestionar la configuración de un servicio dado, permitiendo registrar sus propiedades en archivos de texto y activar/desactivar su monitorización.
+
+La configuración en el directorio indicado en el valor de configuración 'services_data')
+
+=head1 METODOS
+
+=head2 get_name()
+
+=head2 get_script()
+
+=head2 get_pid_file()
+
+=head2 get_process()
+
+=head2 load_config()
+
+Carga la configuración de nuevo desde el fichero correspondiente en /etc/gesapl/services
+
+=cut
